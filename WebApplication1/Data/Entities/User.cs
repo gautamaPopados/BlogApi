d8eae418e1277dc561bffcaf46af5cc726e04005
Egor Morozov <egornexus10@gmail.com>
2024-11-16 22:04:58 +0700

    changing project

---
+++ WebApplication1/Data/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp1.Data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string fullName { get; set; }
+
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
